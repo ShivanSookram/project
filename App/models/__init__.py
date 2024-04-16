30e1dc00317b1aebe5e7b4e1b3145d391210b1cc
Shivan001 <61838605+Shivan001@users.noreply.github.com>
2024-04-16 16:49:36 +0000

    init

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,13 +1,15 @@
 from .user import *
 from .internship import internship
 
-# Read data from the "internship.csv" file
-with open('internship.csv', 'r') as file:
-    data = file.readlines()
+with open('internship.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        internship = Internship(name=row['Internship Name'],
+                                category=row['Category'],
+                                duration=row['Duration'],
+                                location=row['Location'],
+                                description=row['Description'])
+        db.session.add(internship)
+    db.session.commit()
 
-# Store data in the "internship" table
-for line in data:
-    # Assuming each line in the CSV file represents a record with comma-separated values
-    values = line.strip().split(',')
-    internship = internship(*values)
-    internship.save()
+print('database initialized!')
