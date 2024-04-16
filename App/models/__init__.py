26fe01f902d0259d2e185d803a1a919404368d5a
Shivan001 <shivan.sookram@gmail.com>
2024-04-16 18:49:03 +0000

    csv importing

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -4,15 +4,16 @@ import csv
 from App.database import db
 
 with open('internship.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        internship = Internship(internship_title=row['Internship Name'],
-                                company_name=row['Company Name'],
-                                location=row['Location'],
-                                duration=row['Duration'],
-                                stipend=row['Stipend'],
-                                start_date=row['Description'])
-        db.session.add(internship)
-    db.session.commit()
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+    internship = Internship(internship_title=row['internship_title'],
+                             company_name=row['company_name'],
+                             location=row['location'],
+                             start_date=row['start_date'],
+                             duration=row['duration'],
+                             stipend=row['stipend'])
+    
+    db.session.add(internship)
+db.session.commit()
 
 print('database initialized!')
--- App/models/internship.py
+++ App/models/internship.py
@@ -3,21 +3,19 @@ from App.database import db
 class Internship(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     internship_title = db.Column(db.String, nullable=False)
-    company_name = db.Column(db.String, nullable=False) 
+    company_name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
-    start_date = db.Column(db.String, nullable=False)
-    duration = db.Column(db.String, nullable=False)
-    stipend = db.Column(db.String, nullable=False)
+    start_date = db.Column(db.Date, nullable=False)  
+    duration = db.Column(db.String, nullable=False)  
+    stipend = db.Column(db.Float, nullable=False)
 
-    def __init__(self,id, internship_title, company_name, location, start_date, duration, stipend):
+    def __init__(self, internship_title, company_name, location, start_date, duration, stipend):
         self.internship_title = internship_title
         self.company_name = company_name
         self.location = location
         self.start_date = start_date
         self.duration = duration
         self.stipend = stipend
-        self.id = id
 
     def __repr__(self):
         return f"<Internship {self.internship_title} {self.company_name}>"
-    
