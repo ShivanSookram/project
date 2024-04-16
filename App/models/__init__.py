cdb04264984fcd9e27a3e51d82c2740460960913
Shivan001 <61838605+Shivan001@users.noreply.github.com>
2024-04-16 17:24:34 +0000

    init fixed

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -6,11 +6,12 @@ from App.database import db
 with open('internship.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        internship = Internship(name=row['Internship Name'],
-                                category=row['Category'],
-                                duration=row['Duration'],
+        internship = Internship(internship_title=row['Internship Name'],
+                                company_name=row['Company Name'],
                                 location=row['Location'],
-                                description=row['Description'])
+                                duration=row['Duration'],
+                                stipend=row['Stipend'],
+                                start_date=row['Description'])
         db.session.add(internship)
     db.session.commit()
 
