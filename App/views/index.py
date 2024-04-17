c101c02b7efa238f2f599dc1b7f9f550dfd089ea
Shivan001 <shivan.sookram@gmail.com>
2024-04-17 20:00:02 +0000

    fixed working

--- App/views/index.py
+++ App/views/index.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.models import db
+from App.models import Internship
 from App.controllers import create_user
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
+@index_views.route('/home', methods=['GET'])
 def index_page():
   internships = Internship.query.all()
   return render_template('index.html', internships=internships)
