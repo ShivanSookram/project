8d2aad7a9ed7b56b61c9e3d697f52f99c58050bd
v-runnn <varun.maharaj5@my.uwi.edu>
2024-04-19 04:19:41 +0000

    Pagination and search now works cohesively.

--- App/models/user.py
+++ App/models/user.py
@@ -1,26 +1,56 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String(120), nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String, nullable=False, unique=True)
+  password = db.Column(db.String(120), nullable=False)
+  type = db.Column(db.String(50))
+  __mapper_args__ = {'polymorphic_identity': 'user', 'polymorphic_on': type}
 
-    def __init__(self, username, password):
-        self.username = username
-        self.set_password(password)
+  def __init__(self, username, password):
+    self.username = username
+    self.set_password(password)
 
-    def get_json(self):
-        return{
-            'id': self.id,
-            'username': self.username
-        }
+  def get_json(self):
+    return {
+      'id': self.id,
+      'username': self.username
+    }
 
-    def set_password(self, password):
-        """Create hashed password."""
-        self.password = generate_password_hash(password)
-    
-    def check_password(self, password):
-        """Check hashed password."""
-        return check_password_hash(self.password, password)
+  def set_password(self, password):
+    """Create hashed password."""
+    self.password = generate_password_hash(password)
 
+  def check_password(self, password):
+    """Check hashed password."""
+    return check_password_hash(self.password, password)
+
+
+class Admin(User):
+  __tablename__ = 'admin'
+  staff_id = db.Column(db.String(120), unique=True)
+  __mapper_args__ = {'polymorphic_identity': 'admin', }
+
+  def __init__(self, staff_id, username, password):
+    super().__init__(username, password)
+    self.staff_id = staff_id
+
+  def get_json(self):
+    return {
+      "id": self.id,
+      "username": self.username,
+      "staff_id": self.staff_id,
+      "type": self.type
+    }
+
+  def __repr__(self):
+    return f'<Admin {self.id} : {self.username}>'
+
+
+class RegularUser(User):
+  __tablename__ = 'regular_user'
+  __mapper_args__ = {
+    'polymorphic_identity': 'regular user',
+  }
--- App/views/index.py
+++ App/views/index.py
@@ -8,36 +8,42 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 @index_views.route('/', methods=['GET'])
 @index_views.route('/home', methods=['GET'])
 def index_page():
+  search_query = request.args.get('search')
   page = request.args.get('page', 1, type=int)
-  internships = Internship.query
+  if search_query:
+    internships = Internship.query.filter(Internship.internship_title.like("%" + search_query + "%") | Internship.company_name.like("%" + search_query + "%"))
+  else:
+    internships = Internship.query  # Get all internships if no search term
   paged_internship = internships.paginate(page=page, per_page=10)
-  return render_template('index.html', paged_internship=paged_internship, page=page)
+  return render_template('index.html', paged_internship=paged_internship, page=page, search_query=search_query)
 
 
 @index_views.route('/init', methods=['GET'])
 def init():
-    db.drop_all()
-    db.create_all()
-    create_user('bob', 'bobpass')
-    return jsonify(message='db initialized!')
+  db.drop_all()
+  db.create_all()
+  create_user('bob', 'bobpass')
+  return jsonify(message='db initialized!')
 
 @index_views.route('/health', methods=['GET'])
 def health_check():
-    return jsonify({'status':'healthy'})
+  return jsonify({'status':'healthy'})
 
 
-@index_views.route('/search', methods=['GET'])
-def search_internships():
-    search_term = request.args.get('search')
-    if search_term:
-        internships = Internship.query.filter(
-            Internship.internship_title.like(f'%{search_term}%') |
-            Internship.company_name.like(f'%{search_term}%')
-        )
-    else:
-        internships = Internship.query  # Get all internships if no search term
-
-    page = request.args.get('page', 1, type=int)
-    paged_internship = internships.paginate(page=page, per_page=10)
-
-    return render_template('index.html', internships=internships.all(), paged_internship=paged_internship)
+# @index_views.route('/search', methods=['GET'])
+# def search_internships():
+#   page = request.args.get('page', 1, type=int)
+#   search_query = request.args.get('search_query')
+#   search_term = request.args.get('search')
+#   if search_term:
+#     internships = Internship.query.filter(
+#       Internship.internship_title.like(f'%{search_term}%') |
+#       Internship.company_name.like(f'%{search_term}%')
+#     )
+#   else:
+#     internships = Internship.query  # Get all internships if no search term
+#     paged_internship = internships.paginate(page=page, per_page=10)
+#     return render_template('index.html', paged_internship=paged_internship, page=page)
+#   paged_internship = internships.paginate(page=page, per_page=10)
+#
+#   return render_template('search.html', internships=internships.all(), paged_internship=paged_internship, page=page, search_query=search_query)
