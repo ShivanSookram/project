21b963d547cc692e135d8e61ab52db76d3dd625d
Shivan001 <shivan.sookram@gmail.com>
2024-04-22 02:22:02 +0000

    WORKING IN GITPOD

--- App/controllers/internship.py
+++ App/controllers/internship.py
@@ -4,17 +4,12 @@ import csv
 
 
 def initialize_internship():  
-  with open('./App/static/internship.csv', 'r',encoding='utf8') as file:
+  with open('App/static/internship.csv', 'r') as file:
     csv_reader = csv.reader(file)
     next(csv_reader) 
     for row in csv_reader:
       new_internship = Internship(row[0], row[1], row[2], row[3], row[4], row[5])
       db.session.add(new_internship)
-
-
-if __name__ == "__main__":
-  initialize_internship()
-
 
   try:
     db.session.commit()
